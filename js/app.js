52acc9719b4eb978a5ebc5ca4463cc3f5aefc3b8
Benny Katchy <kennethmichael11@gmail.com>
2018-11-09 19:58:58 +0100

    Made changes on the enemy by adding and making the enemy to move in the game board

--- js/app.js
+++ js/app.js
@@ -1,11 +1,17 @@
 // Enemies our player must avoid
-var Enemy = function () {
+var Enemy = function (x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
+    this.x = x;
+    this.y = y + 55;
+    this.speed = speed * 3;
     this.sprite = 'images/enemy-bug.png';
+    this.step = 101;
+    this.boundary = this.step * 5;
+    this.resetEnemy = -this.step;
 };
 
 // Update the enemy's position, required method for game
@@ -14,6 +20,13 @@ Enemy.prototype.update = function (dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    if(this.x < this.boundary) {
+        this.x += this.speed * dt;
+    }
+    else {
+        // to reset the enemy
+        this.x = this.resetEnemy;
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -72,6 +85,11 @@ class Hero {
 }
 
 const player = new Hero();
+const enemy1 = new Enemy(-101, 0, 200);
+const enemy2 = new Enemy(-101, 83, 300);
+const enemy3 = new Enemy((-101*2.5), 83, 300);
+const allEnemies = [];
+allEnemies.push(enemy1,enemy2,enemy3);
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
