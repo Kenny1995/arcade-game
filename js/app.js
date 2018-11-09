d384e892cbb00e4dabedf88221c91e1b20825977
Benny Katchy <kennethmichael11@gmail.com>
2018-11-09 16:54:45 +0100

    created Player(hero) and made it to move round the game board

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function () {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
@@ -10,30 +10,64 @@ var Enemy = function() {
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
+Enemy.prototype.update = function (dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
 };
 
 // Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+Enemy.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 // Now write your own player class
 // This class requires an update(), render() and
-// a handleInput() method.
+
 class Hero {
     constructor() {
-        this.x = 0;
-        this.y = 0;
         this.sprite = 'images/char-boy.png';
+        this.step = 101;
+        this.jump = 83;
+        this.startX = this.step * 2;
+        this.startY = (this.jump * 5) - 20;
+        this.x = this.startX;
+        this.y = this.startY;
     }
 
     // To Draw the hero sprite on current x and y cordinate
     render() {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+
+    /**
+     * Update the x and y properties in hero according to input
+     *
+     * @param {string} input - Direction to travel
+     */
+    handleInput(input) {
+        switch(input) {
+            case 'left':
+                if(this.x > 0) {
+                    this.x -= this.step;
+                }
+                break;
+            case 'up':
+                if(this.y > 0) {
+                    this.y -= this.jump;
+                }
+                break;
+            case 'right':
+                if(this.x < this.step * 4) {
+                    this.x +=this.step;
+                }
+                break;
+            case 'down':
+                if(this.y < this.jump * 4) {
+                    this.y +=this.jump;
+                }
+                break;
+        }
     }
 }
 
@@ -47,7 +81,7 @@ const player = new Hero();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
+document.addEventListener('keyup', function (e) {
     var allowedKeys = {
         37: 'left',
         38: 'up',
