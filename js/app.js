8c98a2605e2fe5392a74fa6acfa0097761b4d329
Benny Katchy <kennethmichael11@gmail.com>
2018-11-10 17:50:47 +0100

    Made changes in the whole part of the game

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+let level = 1;
+
 // Enemies our player must avoid
 var Enemy = function (x, y, speed) {
     // Variables applied to each of our instances go here,
@@ -7,7 +9,7 @@ var Enemy = function (x, y, speed) {
     // a helper we've provided to easily load images
     this.x = x;
     this.y = y + 55;
-    this.speed = speed * 3;
+    this.speed = speed;
     this.sprite = 'images/enemy-bug.png';
     this.step = 101;
     this.boundary = this.step * 5;
@@ -20,10 +22,22 @@ Enemy.prototype.update = function (dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    if(this.x < this.boundary) {
-        this.x += this.speed * dt;
+    if( level === 1){
+        //allEnemies.splice(4, 9);
+        this.speed = 160;
     }
-    else {
+    if( level === 2 ){
+        //allEnemies.splice(6, 9);
+        this.speed = 270;
+    }
+    if( level === 3 ){
+        //allEnemies.slice(0, 9);
+        this.speed = 360;
+    }
+
+    if (this.x < this.boundary) {
+        this.x += this.speed * dt;
+    } else {
         // to reset the enemy
         this.x = this.resetEnemy;
     }
@@ -43,9 +57,10 @@ class Hero {
         this.step = 101;
         this.jump = 83;
         this.startX = this.step * 2;
-        this.startY = (this.jump * 5) - 20;
+        this.startY = (this.jump * 4) + 55;
         this.x = this.startX;
         this.y = this.startY;
+        this.victory = false;
     }
 
     // To Draw the hero sprite on current x and y cordinate
@@ -59,37 +74,73 @@ class Hero {
      * @param {string} input - Direction to travel
      */
     handleInput(input) {
-        switch(input) {
+        switch (input) {
             case 'left':
-                if(this.x > 0) {
+                if (this.x > 0) {
                     this.x -= this.step;
                 }
                 break;
             case 'up':
-                if(this.y > 0) {
+                if (this.y > 0) {
                     this.y -= this.jump;
                 }
                 break;
             case 'right':
-                if(this.x < this.step * 4) {
-                    this.x +=this.step;
+                if (this.x < this.step * 4) {
+                    this.x += this.step;
                 }
                 break;
             case 'down':
-                if(this.y < this.jump * 4) {
-                    this.y +=this.jump;
+                if (this.y < this.jump * 4) {
+                    this.y += this.jump;
                 }
                 break;
         }
     }
+
+    update() {
+
+        for (let enemy of allEnemies) {
+            if (this.y === enemy.y && (enemy.x + enemy.step / 2 > this.x && enemy.x < this.x + this.step / 2)) {
+                this.reset();
+            }
+        }
+
+        // if(this.y === -28) {
+        //     this.victory = true;
+        // }
+        //changes, handles and displays the level
+        if (this.y < -18) {
+            this.reset();
+            level++;
+            if (level > 3) {
+                prompt('YOU WIN!!!')
+                setTimeout(this.reset(), 1000);
+                level = 1;
+            }
+            document.getElementById("myspan").innerHTML = level;
+        }
+    }
+
+    reset() {
+        this.x = this.startX;
+        this.y = this.startY;
+    }
 }
 
 const player = new Hero();
-const enemy1 = new Enemy(-101, 0, 200);
-const enemy2 = new Enemy(-101, 83, 300);
-const enemy3 = new Enemy((-101*2.5), 83, 300);
+const enemy1 = new Enemy(-101, 0, this.speed);
+const enemy2 = new Enemy(-101, 83, this.speed);
+const enemy3 = new Enemy((-101 * 2.5), 83, this.speed);
+const enemy4 = new Enemy(-101, 166, this.speed);
+const enemy5 = new Enemy((-101 * 2.5), 0, this.speed);
+const enemy6 = new Enemy((-101 * 3.5), 83, this.speed);
+const enemy7 = new Enemy((-101 * 2.5), 166, this.speed);
+const enemy8 = new Enemy((-101 * 3.8), 0, this.speed);
+const enemy9 = new Enemy((-101 * 4), 166, this.speed);
+
 const allEnemies = [];
-allEnemies.push(enemy1,enemy2,enemy3);
+allEnemies.push(enemy1, enemy2, enemy3, enemy4, enemy5, enemy6, enemy7, enemy8, enemy9);
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
